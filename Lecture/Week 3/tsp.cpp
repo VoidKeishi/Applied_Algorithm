3e1d9f9a1e1822238483225d2041dfe5087d4f69
VoidKeishi <phananha4@gmail.com>
2023-11-05 00:16:16 +0700

    update

--- Lecture/Week 3/tsp.cpp
+++ Lecture/Week 3/tsp.cpp
@@ -1,51 +1,80 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define INF 1000000000
+#define MAX_N 20
+int n, dist[MAX_N][MAX_N];
 
-int main(){
-    int n;
-    cin >> n;
-    int c[n][n];
-    for (int i=0; i<n; i++){
-        for (int j=0; j<n; j++){
-            cin >> c[i][j];
+// Branch and bound version of TSP
+// Time complexity: O(n!)
+// Space complexity: O(n^2)
+
+bool visited[MAX_N];
+int step[MAX_N];
+int ans = INF;
+int sum = 0;
+int Min = INF;
+
+void Try(int k){
+    for (int i = 0; i < n; i++){
+        if (!visited[i]) {
+            step[k] = i;
+            visited[i] = true;
+            sum += dist[step[k-1]][step[k]];
+            if (k == (n - 1) && sum + dist[step[k]][0] < ans)
+            {
+                ans = sum + dist[step[k]][0];
+            }
+            else if (sum+Min*(n-k+1)<ans){
+                Try(k+1);
+            }
+            visited[i] = false;
+            sum -= dist[step[k - 1]][step[k]];
         }
     }
-    int solution=0;
-
 }
 
-int travllingSalesmanProblem(int graph[][V], int s)
-{
-    vector<int> vertex;
-    for (int i = 0; i < V; i++)
-        if (i != s)
-            vertex.push_back(i);
- 
-    int min_path = INT_MAX;
-    do {
-        int current_pathweight = 0;
-        int k = s;
-        for (int i = 0; i < vertex.size(); i++) {
-            current_pathweight += graph[k][vertex[i]];
-            k = vertex[i];
+int main(){
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++) {
+            cin >> dist[i][j];
+            if (dist[i][j]< Min && i!=j) {
+                Min = dist[i][j];
+            }
         }
-        current_pathweight += graph[k][s];
- 
-        // update minimum
-        min_path = min(min_path, current_pathweight);
- 
-    } while (
-        next_permutation(vertex.begin(), vertex.end()));
- 
-    return min_path;
+        visited[i] = false;
+    }
+    visited[0] = true;
+    step[0] = 0;
+    Try(1);
+    cout << ans << endl;
 }
-int main()
-{
-    int graph[][V] = { { 0, 10, 15, 20 },
-                       { 10, 0, 35, 25 },
-                       { 15, 35, 0, 30 },
-                       { 20, 25, 30, 0 } };
-    int s = 0;
-    cout << travllingSalesmanProblem(graph, s) << endl;
-    return 0;
+
+// Dynamic programming version of TSP using bitmask
+// Time complexity: O(n^2 * 2^n)
+// Space complexity: O(n * 2^n)
+
+int dp[MAX_N][1 << MAX_N];
+
+int tsp(int pos, int mask){
+    if (mask == (1 << n) - 1) return dist[pos][0];
+    if (dp[pos][mask] != -1) return dp[pos][mask];
+    int ans = INF;
+    for (int i = 0; i < n; i++){
+        if ((mask & (1 << i)) == 0){
+            ans = min(ans, dist[pos][i] + tsp(i, mask | (1 << i)));
+        }
+    }
+    return dp[pos][mask] = ans;
 }
+int main(){
+    memset(dp, -1, sizeof(dp));
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++) {
+            cin >> dist[i][j];
+        }
+    }
+    cout << tsp(0, 1) << endl;
+}
+
